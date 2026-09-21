92bdc062cd439fb6f5cbfcc798f3bdfde892ba03
imash1999 <abduxashimoviman@gmail.com>
2026-09-21 15:49:53 +0800

    Airflow

---
+++ dags/rfm_pipeline.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'imash1999',
+    'depends_on_past': False,
+    'start_date': datetime(2026, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    'rfm_segmentation_dag',
+    default_args=default_args,
+    description='Регулярный запуск RFM-сегментации пользователей на PySpark',
+    schedule_interval='0 * * * *',
+    catchup=False,
+) as dag:
+
+    run_rfm_spark = BashOperator(
+        task_id='run_rfm_analysis',
+        bash_command='python /opt/airflow/scripts/spark/rfm_analysis.py',
+    )
+
+    run_rfm_spark
